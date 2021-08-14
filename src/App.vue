09d54d49fa96ec33a28c148d019b3efbf4c05801
Bertil <9045728+bertil@user.noreply.gitee.com>
2021-08-15 01:18:19 +0800

    实现下拉框

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <global-header :user="currentUser"></global-header>
     <column-list :list="list"></column-list>
   </div>
 </template>
@@ -8,6 +9,12 @@
 import { defineComponent } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from '@/components/ColumnList.vue'
+import GlobalHeader,{ UserProps } from '@/components/GlobalHeader.vue'
+const currentUser: UserProps = {
+  isLogin: true,
+  name: 'viki',
+  id: 1
+}
 const testData: ColumnProps[] = [
   {
     id: 1,
@@ -21,15 +28,28 @@ const testData: ColumnProps[] = [
     avatar: 'https://img2.baidu.com/it/u=2860557073,3347083343&fm=26&fmt=auto&gp=0.jpg',
     description: '描述aaaaaa'
   },
+  {
+    id: 1,
+    title: '专栏1',
+    description: '描述'
+  },
+  {
+    id: 2,
+    title: '专栏2',
+    avatar: 'https://img2.baidu.com/it/u=2860557073,3347083343&fm=26&fmt=auto&gp=0.jpg',
+    description: '描述aaaaaa'
+  },
 ]
 export default defineComponent({
   name: 'App',
   components: {
-    ColumnList
+    ColumnList,
+    GlobalHeader
   },
   setup() {
     return {
-      list: testData
+      list: testData,
+      currentUser
     }
   }
 })
