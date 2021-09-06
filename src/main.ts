28d627fca30e659df353efe72e2c0cb74c0ffa12
Bertil <9045728+bertil@user.noreply.gitee.com>
2021-09-06 21:58:39 +0800

    组合action

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ axios.defaults.baseURL = 'http://apis.imooc.com/api'
 axios.interceptors.request.use((config) =>{
     store.commit('setLoading', true)
     config.params = { ...config.params, icode: '8B583DFE08F38B06' }
+    let token = window.localStorage.getItem('token')
+    if(token) {
+        config.headers.token = token; 
+    }
     return config
 })
 axios.interceptors.response.use((config)=>{
@@ -16,6 +20,15 @@ axios.interceptors.response.use((config)=>{
       }, 500)
     return config
 })
+// const payload = {
+//   email: '111@test.com',
+//   password: '111111',
+//   icode: '8B583DFE08F38B06'
+// }
+// axios.post('user/login',payload).then((res)=>{
+//   console.log(res.data);
+  
+// })
 
 const app = createApp(App)
 app.use(router)
